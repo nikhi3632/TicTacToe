da36e36c7f01c700aa7c7e98ba2901170ec12081
anamnikhilesh@gmail.com <anamnikhilesh@gmail.com>
2025-07-20 21:21:37 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by NIKHILESH KUMAR REDDY ANAM on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
